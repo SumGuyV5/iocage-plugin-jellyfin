637872b1308f36131c93fb03371d18ac84d37d80
Richard <richard2891@gmail.com>
2021-11-25 17:26:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+iocage-plugin-jellyfin
+Jellyfin is a Free Software Media System that puts you in control of managing and streaming your media. It is an alternative to the proprietary Emby and Plex, to provide media from a dedicated server to end-user devices via multiple apps.
---
+++ post_install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+IP_ADDRESS=$(ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
+
+VERSION="10.7.7"
+fetch https://github.com/Thefrank/jellyfin-server-freebsd/releases/download/v${VERSION}/jellyfinserver-${VERSION}.pkg
+
+pkg install -y jellyfinserver-$VERSION.pkg
+
+ln -s /usr/local/lib/libsqlite3.so /usr/local/lib/libe_sqlite3
+
+pw user add jellyfin -c jellyfin -u 710 -d /nonexistent -s /usr/bin/nologin
+
+sysrc jellyfinserver_enable=TRUE
+
+sysrc jellyfinserver_user="jellyfin"
+sysrc jellyfinserver_group="jellyfin"
+
+service jellyfinserver restart 2>/dev/null
+
+echo -e "Jellyfin now installed.\n" > /root/PLUGIN_INFO
+echo -e "\nPlease open your web browser and go to http://${IP_ADDRESS}:8096 to configure Jellyfin.\n" >> /root/PLUGIN_INFO
